8ddc7b74b3d15dcb830606d282f25e4b3ee97167
Saber Azzouzi <saber.azzouzi@expensya.com>
2024-02-19 23:08:48 +0100

    Add project files.

---
+++ StudyfiedBackend/Controllers/WeatherForecastController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudyfiedBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly bool weather = true;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public bool Get()
+        {
+            return weather;
+        }
+    }
+}
---
+++ StudyfiedBackendUnitTests/Controller/WeatherForecastControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using StudyfiedBackend.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyfiedBackendUnitTests.Controller
+{
+    public class WeatherForecastControllerTests
+    {
+        private readonly ILogger<WeatherForecastController> _weatherForecastLogger;
+        public WeatherForecastControllerTests() {
+            _weatherForecastLogger = A.Fake<ILogger<WeatherForecastController>>();
+        }
+
+        [Fact]
+        public void getWeatherForecastTest()
+        {
+            //Arrange
+            var controller = new WeatherForecastController(_weatherForecastLogger);
+
+            //Act
+            var result = controller.Get();
+
+            //Assert
+            result.Should().BeTrue();
+
+        }
+    }
+}
