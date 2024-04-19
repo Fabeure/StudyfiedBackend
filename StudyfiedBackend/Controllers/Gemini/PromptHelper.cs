1343ce1eca91124de9dd36d57a09c35b37a777b4
Saber Azzouzi <saber.azzouzi@expensya.com>
2024-04-19 15:11:44 +0100

    add prompts

--- StudyfiedBackend/Controllers/Gemini/PromptHelper.cs
+++ StudyfiedBackend/Controllers/Gemini/PromptHelper.cs
@@ -9,7 +9,7 @@
             @" each question and detailled answer should be seperated by a ':', and each pair of question+answer should be seperated by a ';'. Please
             do not include any return to lines, and give me the question, followed by a ':', followed by the answer, followed by a ';' and then the next question answer pair so on and so on
             of course replace question and aswer with the actual question and actual answer
-            DO NOT FORMAT THE RESPONSE IN ANY OTHER WAY, DO NOT WRITE THE WORD QUESTION OR ANSWER FOR ME",
+            DO NOT FORMAT THE RESPONSE IN ANY OTHER WAY, DO NOT WRITE THE WORD QUESTION OR ANSWER FOR ME. The first part should of the flashcard is a question, the second is an answer to that question.",
             "**Context: This image is a page from a study material. Task: If the image appears relevant to a course topic, summarize the key points and concepts.Use the language related to the content . Otherwise, indicate that the content is not relevant.",
             "based on this paragraph , summarize the key points and concepts. i want it formatted using html tags and style using css.include only the html tags for the content along with required style required without the header part and make sure to add onmy the topic of its content in <h2> tag..here is ther paragraph:"
         };
