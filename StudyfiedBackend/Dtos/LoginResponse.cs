e002dd5fbc3dd42ff89ddf959b6ea518c573691c
Saber Azzouzi <saber.azzouzi@expensya.com>
2024-06-01 14:07:49 +0100

    add name to login result

--- StudyfiedBackend/Dtos/LoginResponse.cs
+++ StudyfiedBackend/Dtos/LoginResponse.cs
@@ -7,5 +7,6 @@
         public string Email { get; set; } = string.Empty;
         public string UserId { get; set; } = string.Empty;
         public string Message { get; set; } = string.Empty;
+        public string Name {  get; set; } = string.Empty;
     }
 }
