d798048f62f84575674a0dd48c99d86b4364c9eb
Saber Azzouzi <saber.azzouzi@expensya.com>
2024-06-03 00:22:41 +0100

    add chatbot endpoint

---
+++ StudyfiedBackend/Controllers/ChatBot/ChatBotService.cs
@@ -0,0 +1,28 @@
+﻿using DotnetGeminiSDK.Client.Interfaces;
+using StudyfiedBackend.BaseResponse;
+using StudyfiedBackend.Controllers.Gemini;
+using StudyfiedBackend.Models;
+
+namespace StudyfiedBackend.Controllers.ChatBot
+{
+    public class ChatBotService : IChatBotService
+    {
+        private readonly IGeminiClient _geminiClient;
+
+        public ChatBotService(IGeminiClient geminiClient)
+        {
+            _geminiClient = geminiClient;
+        }
+
+        public BaseResponse<string> getChatResponse(string conversation)
+        {
+            conversation = PromptHelper.addHelperToPrompt(conversation, 4, 0);
+
+            var geminiResponse = GenericGeminiClient.GetTextPrompt(_geminiClient, conversation).Result;
+
+            string responseContents = geminiResponse.Candidates[0].Content.Parts[0].Text;
+
+            return new BaseResponse<string>(ResultCodeEnum.Success, responseContents, "Successfully fetched response");
+        }
+    }
+}
---
+++ StudyfiedBackend/Controllers/ChatBot/IChatBotService.cs
@@ -0,0 +1,8 @@
+﻿using StudyfiedBackend.BaseResponse;
+namespace StudyfiedBackend.Controllers.ChatBot
+{
+    public interface IChatBotService
+    {
+        public BaseResponse<string> getChatResponse(string conversation);
+    }
+}
